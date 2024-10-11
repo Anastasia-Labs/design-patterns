f0f805872ddb71de078882d7aabae38de218de1c
colll78 <philipdisarro@gmail.com>
2024-10-11 08:25:30 -0700

    Update ListLength.hs

--- utxo-indexers/examples/ListLength.hs
+++ utxo-indexers/examples/ListLength.hs
@@ -15,14 +15,15 @@ genInlinedTails n xs = foldr (\_ acc -> [| BI.tail $acc |]) (varE xs) [1..n]
 -- Recursively compute the count of elements in a list (the count starts at `acc`)
 -- ie: builtinListLength 3 [1,2] == 5 
 -- the list has a length of 2 and `acc` is 3, so we have 2 + 3 = 5.
-builtinListLength :: forall a. Integer -> BI.BuiltinList a -> Integer
+builtinListLength :: Integer -> BI.BuiltinList a -> Integer
 builtinListLength acc xs = 
-  BI.chooseList xs
-      -- if the list is empty we return `acc` 
-      acc
-      -- if the list is non-empty we increase the counter and recurse
-      (\_ ys -> builtinListLength (acc + 1) ys)
-      ()
+  -- matchList is a builtin function that takes a list and branches depending on whether the list is empty or not
+  -- it expects the non-empty branch argument to be a function (\x xs -> ..) where x is the head of the list and xs is the tail.
+  BI.matchList xs
+    -- if the list is empty we return `acc` 
+    acc
+    -- if the list is non-empty we increase the counter and recurse
+    (\_ ys -> go (acc + 1) ys)
 
 -- An efficient length function that takes as a parameter the expected length and uses that knowledge
 -- to avoid unnecessary recursion and checks. 
@@ -34,4 +35,8 @@ builtinListLengthFast n xs = go n 0 xs
     | 30 <= remainingExpected = go (remainingExpected - 30) (currentCount + 30) $(genInlinedTails 30 'xs)
     | 20 <= remainingExpected = go (remainingExpected - 20) (currentCount + 20) $(genInlinedTails 20 'xs)
     | 10 <= remainingExpected = go (remainingExpected - 10) (currentCount + 10) $(genInlinedTails 10 'xs)
-    | otherwise = builtinListLength 0 xs
+    -- with less than 10 elements we revert to chooseList because the overhead of builtinAddInteger, builtinIfThenElse and builtinLessThanEquals
+    -- is more inefficient than simply recursing with matchList. Although it may be more efficient to falling back to a function that 
+    -- drops multiple elements at a time (3-4) at which point the aforementioned overhead might be compensated for by the reduced number of recursive calls.
+    | otherwise = builtinListLength currentCount xs
+
